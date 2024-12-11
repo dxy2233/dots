43a45258d5c0989cd1b8ea143c482d44e99b42dd
dxy2233 <691369338@qq.com>
2024-12-11 08:32:40 +0800

    升级依赖等

--- configs/nvim/lua/plugins/codeium.lua
+++ configs/nvim/lua/plugins/codeium.lua
@@ -1,45 +1,35 @@
 return {
+  {
+    "nvim-cmp",
+    optional = true,
+    dependencies = { "codeium.nvim" },
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "codeium",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("codeium"))
+    end,
+  },
   -- {
-  --   "Exafunction/codeium.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "hrsh7th/nvim-cmp",
-  --   },
-  --   config = function()
-  --     require("codeium").setup({})
-  --   end,
-  -- },
-  -- {
-  --   "nvim-cmp",
-  --   dependencies = {
-  --     -- codeium
-  --     {
-  --       "Exafunction/codeium.nvim",
-  --       cmd = "Codeium",
-  --       build = ":Codeium Auth",
-  --       opts = {},
+  --   "saghen/blink.cmp",
+  --   optional = true,
+  --   opts = {
+  --     sources = {
+  --       compat = vim.g.ai_cmp and { "codeium" } or nil,
   --     },
   --   },
-  --   ---@param opts cmp.ConfigSchema
-  --   opts = function(_, opts)
-  --     table.insert(opts.sources, 1, {
-  --       name = "codeium",
-  --       group_index = 1,
-  --       priority = 100,
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   optional = true,
-  --   event = "VeryLazy",
-  --   opts = function(_, opts)
-  --     table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("codeium"))
-  --   end,
-  -- },
-
-  -- {
-  --   "Exafunction/codeium.nvim",
-  --   enabled = false,
+  --   dependencies = {
+  --     "codeium.nvim",
+  --     vim.g.ai_cmp and "saghen/blink.compat" or nil,
+  --   },
   -- },
 }
